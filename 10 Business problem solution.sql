4d9032f176f3c0c6a98c507d4f5ee038d673cdbe
rishabh590-ship-it <rishabh590rko@gmail.com>
2026-08-08 08:54:12 +0530

    Update 10 Business problem solution.sql

--- 10 Business problem solution.sql
+++ 10 Business problem solution.sql
@@ -57,3 +57,34 @@ Create View Incomplete_Rides_Reason As
 SELECT Booking_ID, Incomplete_Rides_Reason
 FROM bookings
 WHERE Incomplete_Rides = 'Yes';
+
+
+#1. Retrieve all successful bookings:
+Select * From Successful_Bookings;
+
+#2. Find the average ride distance for each vehicle type:
+Select * from ride_distance_for_each_vehicle;
+
+#3. Get the total number of cancelled rides by customers:
+Select * from cancelled_rides_by_customers;
+
+#4. List the top 5 customers who booked the highest number of rides:
+Select * from Top_5_Customers;
+
+#5. Get the number of rides cancelled by drivers due to personal and car-related issues:
+Select * from Rides_cancelled_by_Drivers_P_C_Issues;
+
+#6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
+Select * from Max_Min_Driver_Rating;
+
+#7. Retrieve all rides where payment was made using UPI:
+Select * from UPI_Payment;
+
+#8. Find the average customer rating per vehicle type:
+Select * from AVG_Cust_Rating;
+
+#9. Calculate the total booking value of rides completed successfully:
+Select * from total_successful_ride_value;
+
+#10. List all incomplete rides along with the reason:
+Select * from Incomplete_Rides_Reason
